507688b63e0572d02085376d2cf0372b9fc936d8
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-05-16 12:26:23 +0300

    fix

--- microfrontends/module-federation/projects/shell/src/app/app-routing.module.ts
+++ microfrontends/module-federation/projects/shell/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { HiModule } from './../../../mf1/src/app/hi/hi.module';
+import { HiComponent } from './../../../mf1/src/app/hi/hi.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
@@ -6,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: ShellComponent,
+  },
+  {
+    path: 'mf1',
+    loadChildren: () => import('mf/HiModule').then(m => m.HiModule),
   }
 ];
 
